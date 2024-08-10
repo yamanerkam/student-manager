0e15a1592e1deff84650619453bccd52a828a08f
Kam <erkamyaman35@gmail.com>
2024-08-10 22:48:41 +0300

    signup-page

--- client/src/app/pages/signup/signup.component.ts
+++ client/src/app/pages/signup/signup.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import { NgModule } from '@angular/core';
+
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule,
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
