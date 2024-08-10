47ce3a7d05737912300e4ea9723b8b042dd1b858
Kam <erkamyaman35@gmail.com>
2024-08-10 22:45:58 +0300

    login-page-added

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FloatLabelModule,
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
