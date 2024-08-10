635c0812628984450fe1b45a4b91e63c67006c7b
Kam <erkamyaman35@gmail.com>
2024-08-10 21:59:50 +0300

    simple-routing-and-pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ToDoComponent } from './pages/to-do/to-do.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'signup', component: SignupComponent },
+    { path: 'home', component: HomepageComponent },
+    { path: 'todo', component: ToDoComponent },
+    { path: '**', component: NotFoundComponent },
+
+
+];
