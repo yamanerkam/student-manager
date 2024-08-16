a119441306d5238cb22d2bc64cd53043e3f8acff
Kam <erkamyaman35@gmail.com>
2024-08-16 21:22:11 +0300

    student-card-start

---
+++ client/src/app/components/student-card/student-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-card.component.html',
+  styleUrl: './student-card.component.css'
+})
+export class StudentCardComponent {
+
+}
