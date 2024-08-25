cea7e0a7fbda9d1e1407693ed37072f795b4f574
yamanerkam <erkamyaman35@gmail.com>
2024-08-25 15:49:57 +0300

    url-with-router-and-pipe

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,23 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Kam Teacher';
+  url = ""
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+
+  }
+  ngOnInit() {
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      console.log(this.router.url);
+      this.url = this.router.url
+    });
+
+
+
+  }
+
 }
