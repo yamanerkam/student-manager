75496b9330911cb38fce605c83d985e419268fb6
yamanerkam <erkamyaman35@gmail.com>
2024-08-25 16:49:14 +0300

    sidebar-activation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,27 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'Kam Teacher';
   url = ""
+  sidebarActive: boolean = true
   constructor(private router: Router, private activatedRouter: ActivatedRoute) {
 
   }
   ngOnInit() {
-
+    this.sidebarActive = true
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
       console.log(this.router.url);
       this.url = this.router.url
+      if (this.url == "/signup" || this.url == "/login") {
+        this.sidebarActive = false
+      }
     });
 
 
--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -5,19 +5,20 @@ import { RippleModule } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { Sidebar } from 'primeng/sidebar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule],
+  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
-  closeCallback(e:any): void {
-      this.sidebarRef.close(e);
+  closeCallback(e: any): void {
+    this.sidebarRef.close(e);
   }
 
   sidebarVisible: boolean = false;
