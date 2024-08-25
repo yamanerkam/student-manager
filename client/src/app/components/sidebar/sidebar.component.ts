69d5ef30f8fc75aad68f4e445c767a388ce3f757
yamanerkam <erkamyaman35@gmail.com>
2024-08-25 17:14:16 +0300

    sidebar-routing-starts

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -6,11 +6,13 @@ import { AvatarModule } from 'primeng/avatar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { Sidebar } from 'primeng/sidebar';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, CommonModule],
+  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
