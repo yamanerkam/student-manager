8cb97de795b139c4900f10b1350d66a7506931ef
yamanerkam <erkamyaman35@gmail.com>
2024-08-26 22:01:02 +0300

    search-bar-start

--- client/src/app/pages/student-card-screen/student-card-screen.component.ts
+++ client/src/app/pages/student-card-screen/student-card-screen.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
 import { Student } from '../../components/student-card/student-card.component';
 
 import { StudentCardComponent } from '../../components/student-card/student-card.component';
 import * as studentData from '../../../assets/students.json'
+
+
 @Component({
   selector: 'app-student-card-screen',
   standalone: true,
-  imports: [StudentCardComponent, CommonModule],
+  imports: [StudentCardComponent, CommonModule, ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule],
   templateUrl: './student-card-screen.component.html',
   styleUrl: './student-card-screen.component.css'
 })
