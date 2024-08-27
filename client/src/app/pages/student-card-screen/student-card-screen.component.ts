613d0f8e1d9049c014880d6975b0ee316770860d
yamanerkam <erkamyaman35@gmail.com>
2024-08-27 18:53:58 +0300

    feature/ filtering by grade

--- client/src/app/pages/student-card-screen/student-card-screen.component.ts
+++ client/src/app/pages/student-card-screen/student-card-screen.component.ts
@@ -11,39 +11,63 @@ import { Student } from '../../components/student-card/student-card.component';
 import { StudentCardComponent } from '../../components/student-card/student-card.component';
 import * as studentData from '../../../assets/students.json'
 import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
+type Grade = {
+  Grade:number
+}
 
 @Component({
   selector: 'app-student-card-screen',
   standalone: true,
-  imports: [StudentCardComponent, FormsModule, CommonModule, ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule],
+  imports: [StudentCardComponent,DropdownModule,FormsModule,CommonModule, ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule],
   templateUrl: './student-card-screen.component.html',
   styleUrl: './student-card-screen.component.css'
 })
-export class StudentCardScreenComponent {
+export class StudentCardScreenComponent implements OnInit {
 
   // add this to your library
   students: Student[] = (studentData as any).default
   studentsFiltered: Student[] = []
   searchTermTemp = ""
-
+  grade: Grade | undefined;
+  grades: Grade[] | undefined = [
+    { Grade: 1 },
+    { Grade: 2 },
+    { Grade: 3 },
+    { Grade: 4 },
+    { Grade: 5 },
+    { Grade: 6 },
+    { Grade: 7 },
+    { Grade: 8 },
+    { Grade: 9 },
+    { Grade: 10 },
+    { Grade: 11 },
+    { Grade: 12 },
+  ];
 
   execute() {
     this.studentsFilter(this.searchTermTemp)
   }
 
-
   constructor() {
     this.studentsFiltered = this.students;
   }
-
+  ngOnInit() {
+    console.log(this.grades)
+  }
 
   studentsFilter(name: string) {
     this.studentsFiltered = this.students.filter((stu) => stu.name.toLowerCase().includes(name.toLowerCase()));
     console.log(this.studentsFiltered);
   }
 
+  studentsFilterByGrade(grade:Grade|undefined){
+    this.studentsFiltered = this.students.filter((stu) => stu.grade == this.grade?.Grade);
+    console.log(this.studentsFiltered); 
+   
 
 
+  }
 
 }
